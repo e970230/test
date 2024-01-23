51c5cce5b2a349d0d368e897faaba27cf048d1b5
e970230 <e970230@gmail.com>
2024-01-23 23:03:34 +0800

    actest_error

--- actest.m
+++ actest.m
@@ -8,35 +8,36 @@ state_dim = 5;    % 替換為你的狀態維度
 
 % 建立演員模型
 actor = [
-    featureInputLayer(state_dim, 'Normalization', 'none', 'Name', 'state')
+    featureInputLayer(state_dim, 'Normalization', 'none', 'Name', 'actor_state')
     fullyConnectedLayer(128, 'Name', 'actor_fc1', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
     reluLayer('Name', 'actor_relu1')
     fullyConnectedLayer(64, 'Name', 'actor_fc2', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
     reluLayer('Name', 'actor_relu2')
-    fullyConnectedLayer(num_actions, 'Name', 'output', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
+    fullyConnectedLayer(num_actions, 'Name', 'actor_output', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
     softmaxLayer('Name', 'softmax')
 ];
 
 % 建立評論家模型
 critic = [
-    featureInputLayer(state_dim, 'Normalization', 'none', 'Name', 'state')
+    featureInputLayer(state_dim, 'Normalization', 'none', 'Name', 'critic_state')
     fullyConnectedLayer(128, 'Name', 'critic_fc1', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
     reluLayer('Name', 'critic_relu1')
     fullyConnectedLayer(64, 'Name', 'critic_fc2', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
     reluLayer('Name', 'critic_relu2')
-    fullyConnectedLayer(1, 'Name', 'output', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
+    fullyConnectedLayer(1, 'Name', 'critic_output', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
 ];
 
 % 連接演員和評論家模型
-actor_input = 'state';
-critic_input = 'state';
+actor_input = 'input_state';
+critic_input = 'critic_state';
 
 actor_critic = layerGraph(actor);
+%%
 actor_critic = addLayers(actor_critic, critic);
 
 % 定義連接
-actor_critic = connectLayers(actor_critic, 'softmax', 'fc1', critic_input);
-actor_critic = connectLayers(actor_critic, 'state', 'state');
+% actor_critic = connectLayers(actor_critic, 'softmax', 'critic_fc1');
+% actor_critic = connectLayers(actor_critic, 'actor_state', 'critic_state');
 
 % 編譯模型
 options = rlRepresentationOptions('LearnRate', 0.01);
