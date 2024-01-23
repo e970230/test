fa6045ff0a3f21fd77ceec35bb33e3312de4ade4
e970230 <e970230@gmail.com>
2024-01-23 21:11:59 +0800

    actest_daily_update

--- actest.m
+++ actest.m
@@ -9,10 +9,10 @@ state_dim = 5;    % 替換為你的狀態維度
 % 建立演員模型
 actor = [
     featureInputLayer(state_dim, 'Normalization', 'none', 'Name', 'state')
-    fullyConnectedLayer(128, 'Name', 'fc1', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
-    reluLayer('Name', 'relu1')
-    fullyConnectedLayer(64, 'Name', 'fc2', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
-    reluLayer('Name', 'relu2')
+    fullyConnectedLayer(128, 'Name', 'actor_fc1', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
+    reluLayer('Name', 'actor_relu1')
+    fullyConnectedLayer(64, 'Name', 'actor_fc2', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
+    reluLayer('Name', 'actor_relu2')
     fullyConnectedLayer(num_actions, 'Name', 'output', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
     softmaxLayer('Name', 'softmax')
 ];
@@ -20,10 +20,10 @@ actor = [
 % 建立評論家模型
 critic = [
     featureInputLayer(state_dim, 'Normalization', 'none', 'Name', 'state')
-    fullyConnectedLayer(128, 'Name', 'fc1', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
-    reluLayer('Name', 'relu1')
-    fullyConnectedLayer(64, 'Name', 'fc2', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
-    reluLayer('Name', 'relu2')
+    fullyConnectedLayer(128, 'Name', 'critic_fc1', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
+    reluLayer('Name', 'critic_relu1')
+    fullyConnectedLayer(64, 'Name', 'critic_fc2', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
+    reluLayer('Name', 'critic_relu2')
     fullyConnectedLayer(1, 'Name', 'output', 'WeightsInitializer', 'glorot', 'BiasInitializer', 'zeros')
 ];
 
