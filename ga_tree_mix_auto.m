5f95e388ffa0a31ba0e2c9a9bc3566b747509b21
e970230 <e970230@gmail.com>
2024-03-12 17:12:57 +0800

    全部資料分析

--- ga_tree_mix_auto.m
+++ ga_tree_mix_auto.m
@@ -13,7 +13,7 @@ rpm_1400=first_data(find(first_data==1400),:);
 rpm_1500=first_data(find(first_data==1500),:);
 rpm_2500=first_data(find(first_data==2500),:);
 
-AA=[120 680 890 1100 1400 1500 2500];
+
 
 
 
@@ -48,13 +48,20 @@ input_data=[rpm_120(rpm_120_rand(randomly_selected_samples+1:end),2:11);rpm_680(
 input_data_answer=[rpm_120(rpm_120_rand(randomly_selected_samples+1:end),1);rpm_680(rpm_680_rand(randomly_selected_samples+1:end),1);rpm_890(rpm_890_rand(randomly_selected_samples+1:end),1);rpm_1100(rpm_1100_rand(randomly_selected_samples+1:end),1);rpm_1400(rpm_1400_rand(randomly_selected_samples+1:end),1);rpm_1500(rpm_1500_rand(randomly_selected_samples+1:end),1);rpm_2500(rpm_2500_rand(randomly_selected_samples+1:end),1)];
 
 
+%%
+
+train_data_all=first_data(:,2:end);
+train_data_all_y=first_data(:,1);
+
+
 % x=[1 1 1];
 
 % 設定基因演算法的初始參數
-PopulationSize=100;     %族群大小意旨染色體數目
-FitnessLimit=1000;      %目標函數跳出門檻(此題為小於此值則演算法結束
+PopulationSize=10;     %族群大小意旨染色體數目
+FitnessLimit=20;      %目標函數跳出門檻(此題為小於此值則演算法結束
 options = optimoptions('ga', 'Display', 'iter', 'PopulationSize', PopulationSize, ...
-    'PlotFcn',{'gaplotbestf','gaplotbestindiv','gaplotexpectation'},'FitnessLimit',FitnessLimit);
+    'PlotFcn',{'gaplotbestf','gaplotbestindiv','gaplotexpectation'},'FitnessLimit',FitnessLimit, ...
+    'Generations',100);
 %'iter'顯示出每次跌代的詳細資訊
 %'PlotFcn'畫圖指令
 % gaplotbestf紀錄每次跌代中最佳答案的分數(fitness)
@@ -64,14 +71,22 @@ options = optimoptions('ga', 'Display', 'iter', 'PopulationSize', PopulationSize
 
 % 定義要優化的參數範圍
 numVariables = 3; % 三個參數
-lb = [1, 1, 1];  % 下限
-ub = [40, 20, 20]; % 上限
+lb = [1, 1, 10];  % 下限
+ub = [80, 30, 30]; % 上限
 
 % 定義訓練數據和驗證數據
-trainData =train_data; % 訓練數據
-trainLabels =train_data_y; % 訓練標籤
-validData = input_data; % 驗證數據
-validLabels = input_data_answer; % 驗證標籤
+% trainData =train_data; % 訓練數據
+% trainLabels =train_data_y; % 訓練標籤
+% validData = input_data; % 驗證數據
+% validLabels = input_data_answer; % 驗證標籤
+
+
+%%
+%全部資料集和標籤
+trainData =train_data_all; % 訓練數據
+trainLabels =train_data_all_y; % 訓練標籤
+validData = train_data_all; % 驗證數據
+validLabels = train_data_all_y; % 驗證標籤
 
 
 % fitness=RandomForestFitness(x, trainData, trainLabels, validData, validLabels);
@@ -91,7 +106,8 @@ fitnessFunction = @(x) RandomForestFitness(x, trainData, trainLabels, validData,
 %[output]輸出運算法的結構
 %[population]最後一次跌代所有染色體的解答
 %[scores]最後一次跌代所有染色體的分數
-
+%Func-count調動目標函數的次數
+%stall generations疊代沒有優化的次數
 
 %%
 % 最優參數
