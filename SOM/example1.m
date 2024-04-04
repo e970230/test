c8acfe7026d72080d5bd42e25c16e859d3b9efe0
e970230 <148304931+e970230@users.noreply.github.com>
2024-04-04 15:20:08 +0800

    SOM範例檔案更新

--- SOM/example1.m
+++ SOM/example1.m
@@ -3,32 +3,19 @@ clc
 close all
 
 % load training data; 2 fatures, 1000 samples.
-data = simplecluster_dataset;
+% data = simplecluster_dataset;
 
-%%
-% rng(5); % 設置隨機數種子，以確保結果可重複
-% 
-% % 生成第一個數據集：x 範圍 -10 到 -5，y 範圍 -10 到 -5
-% data1 = [(-5 + (-10 + 5) * rand(1, 100)); (-5 + (-10 + 5) * rand(1, 100))];
-% 
-% % 生成第二個數據集：x 範圍 -10 到 -5，y 範圍 5 到 10
-% data2 = [(-5 + (-10 + 5) * rand(1, 100)); (5 + (10 - 5) * rand(1, 100))];
-% 
-% % 生成第三個數據集：x 範圍 5 到 10，y 範圍 5 到 10
-% data3 = [(5 + (10 - 5) * rand(1, 100)); (5 + (10 - 5) * rand(1, 100))];
-% 
-% % 生成第四個數據集：x 範圍 5 到 10，y 範圍 -10 到 -5
-% data4 = [(5 + (10 - 5) * rand(1, 100)); (-5 + (-10 + 5) * rand(1, 100))];
-% % 整合成一個矩陣
-% data = [data1 data2 data3 data4];
+load("PCA_test_data.txt");
+data=PCA_test_data(:,1:4);
+data=data';
 
-%%
 
-figure(1) % scatter plot of original dataset
-plot(data(1,:),data(2,:),'bo')
-xlabel('feature 1')
-ylabel('feature 2')
-title('input data (before z-score)')
+
+% figure(1) % scatter plot of original dataset
+% plot(data(1,:),data(2,:),'bo')
+% xlabel('feature 1')
+% ylabel('feature 2')
+% title('input data (before z-score)')
 
 
 % normalized the training dataset by z-score
@@ -37,18 +24,18 @@ dim=2;
 %For example, normalize(A,2) normalizes each row.
 x = normalize(data,dim); % dataset after z-score
 
-figure(2) % scatter plot of dataset after z-score
-plot(x(1,:),x(2,:),'bo')
-xlabel('feature 1')
-ylabel('feature 2')
-title('input data (after z-score)')
+% figure(2) % scatter plot of dataset after z-score
+% plot(x(1,:),x(2,:),'bo')
+% xlabel('feature 1')
+% ylabel('feature 2')
+% title('input data (after z-score)')
 
 
 % setting parameters of SOM training
-dimensions=[10 10];  %dimensions setting in SOM: 10 by 10 network
+dimensions=[8 8];  %dimensions setting in SOM: 10 by 10 network
 numNeuron=dimensions(1)*dimensions(2);   %number of neurons in SOM
 coverSteps=100;    %Number of training steps (default=100)
-initNeighbor=5;    %Initial neighborhood size, default=3
+initNeighbor=1;    %Initial neighborhood size, default=3
 topologyFcn='hextop';   %Layer topology function: hexagon
 distFcn='dist';   %Neuron distance function: Euclidean distance
 
@@ -69,9 +56,9 @@ for k=1:size(x,1)
     weight=[weight ; (wb(((k-1)*numNeuron+1):numNeuron*k))'];
 end
 
-
+%%
 % For a specific input data point, find its BMU in SOM
-input_data=x(:,200);   % a specific data point from dataset x
+input_data=x(:,4);   % a specific data point from dataset x
 isBMU = SOM(input_data); % find the BMU in SOM
 BMU_inx=find(isBMU==1);   % isBMU=1: is BMU ; isBMU=0: not BMU
 BMU_vector=weight(:,BMU_inx);  % weight vector of BMU
