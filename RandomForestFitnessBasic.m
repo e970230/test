ef651b7690e494686549cd54624cdb043bf20b10
e970230 <e970230@gmail.com>
2024-04-09 15:30:07 +0800

    基本森林副程式

---
+++ RandomForestFitnessBasic.m
@@ -0,0 +1,28 @@
+function fitness = RandomForestFitnessBasic(params, trainData, trainLabels, validData, validLabels)
+    % params: 要優化的隨機森林參數，例如決策樹數量、最大深度等
+    % trainData: 訓練數據
+    % trainLabels: 訓練數據對應標籤
+    % validData: 驗證數據
+    % validLabels: 驗證答案
+    % nov: 所選擇的特徵
+    % 使用優化後的參數訓練隨機森林模型
+
+    numTrees = params(1,1);
+    maxDepth = params(1,2);
+    minLeafSize = params(1,3);
+    %下面會從第四項開始是因為基因演算法在疊代時會將前面三個超參數設定一起綁在所選特徵數裡一起疊代
+    %固要跳過前三項從第四項開始
+    
+
+    treeBaggerModel = TreeBagger(numTrees, trainData, trainLabels, 'Method', 'regression', ...
+        'MaxNumSplits', maxDepth, 'MinLeafSize', minLeafSize);
+    
+    % 在驗證集上進行預測
+    predictions = predict(treeBaggerModel, validData);
+    
+    % 計算預測性能指標
+    MSE = mean((predictions - validLabels).^2);
+    
+    % 將目標函數設置為最小化的MSE
+    fitness = MSE;
+end
--- ga_mix_tree_answer.m
+++ ga_mix_tree_answer.m
@@ -15,16 +15,16 @@ trainLabels =train_data_all_y; % 訓練標籤
 validData = train_data_all; % 驗證數據
 validLabels = train_data_all_y; % 驗證標籤
 
-x1_range=[100 400];
-x2_range=[5 100];
-x3_range=[2 2];
+x1_range=[10 200];
+x2_range=[5 50];
+x3_range=[5 10];
 
 tic
 for i=x1_range(1):x1_range(2)
     for y=x2_range(1):x2_range(2)
         for z=x3_range(1):x3_range(2)
             x=[i y z];
-            fitness(i-x1_range(1)+1,y-x2_range(1)+1,z-x3_range(1)+1) =RandomForestFitness(x, trainData, trainLabels, validData, validLabels);
+            fitness(i-x1_range(1)+1,y-x2_range(1)+1,z-x3_range(1)+1) =RandomForestFitnessBasic(x, trainData, trainLabels, validData, validLabels);
             disp(['隨機森林裡決策樹的數量 = ', num2str(i), ', 每顆樹最大的分割次數 = ', num2str(y), ', 葉節點最小樣本數 ', num2str(z)]);
             disp(['適性值分數 = ' , num2str(fitness(i-x1_range(1)+1,y-x2_range(1)+1,z-x3_range(1)+1))])
         end
