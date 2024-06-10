17da69a85821328d29872703d66f4eaf31734db7
e970230 <148304931+e970230@users.noreply.github.com>
2024-06-11 00:38:01 +0800

    數據平滑化註解

--- SG/sg_test.m
+++ SG/sg_test.m
@@ -7,15 +7,15 @@ load AIDC_data_no4_T5.mat
 x=1:length(T5);
 
 
-order = 3;
-framelen = 93;
+order = 3;      %濾波器的階數，此值須小於移動窗口的長度
+framelen = 93;  %移動窗口的長度(必須為奇數
 
 y = sgolayfilt(T5,order,framelen);
 
 
 figure(1)
 hold on 
-plot(x,T5,"Color","R")
-plot(x,y,"Color","G")
+plot(x,T5,"Color","B")
+plot(x,y,"Color","R")
 hold off
-
+legend('原始訊號','降噪後訊號')
