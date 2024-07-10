88076d09141acda8703aa529e2a70fec3976571a
e970230 <e970230@gmail.com>
2024-07-10 09:00:26 +0800

    python的kfold方法1新增及測試

--- python/tree/test_kfold.py
+++ python/tree/test_kfold.py
@@ -13,6 +13,51 @@ feature_dataset = mat['feature_dataset']
 data = feature_dataset[:, 1:]  # 擷取原數據的特徵，第0列為標籤所以特徵從第1列開始擷取
 label = feature_dataset[:, 0]  # 擷取原數據的標籤，為原數據的第0列
 
+def Customized_kfold(data,label):
+    unique_numbers = np.unique(label)
+    for fold in unique_numbers:
+    
+        # 切分數據
+        X_train, X_test = data[np.where(label != fold)[0]], data[np.where(label == fold)[0]]
+        y_train, y_test = label[np.where(label != fold)[0]], label[np.where(label == fold)[0]]
+    return X_train,X_test,y_train,y_test
+
+
+unique_numbers = np.unique(label)
+
+
+all_mse = []
+# 自行實現KFold 交叉驗證
+for fold in unique_numbers:
+    
+    X_train, X_test = data[np.where(label != fold)[0]], data[np.where(label == fold)[0]]
+    y_train, y_test = label[np.where(label != fold)[0]], label[np.where(label == fold)[0]]
+    # 創建 ExtraTreesRegressor 模型
+    model = ExtraTreesRegressor(n_estimators=200,          #將第一個解作為樹的數量
+                                max_features=25,          #將第二個解作為分裂時考慮的最大特徵數
+                                min_samples_split=10,     #將第三個解作為葉節點最小樣本數
+                                random_state=42)
+    # 使用模型進行預測
+    model.fit(X_train, y_train)                 #訓練模型
+    y_pred = model.predict(X_test)              #預測解答
+
+    # 計算預測答案和原始標籤之MSE值
+    mse = mean_squared_error(y_test, y_pred)    #計算MSE值
+    all_mse.append(mse)                         #將當次MSE值記錄下來
+    
+final_mse_mean = np.mean(all_mse, axis=0)
+    
+
+print(f"\n{np.shape(unique_numbers)} 折交叉驗證的平均準確度: {final_mse_mean}")
+
+
+
+
+
+
+
+'''
+
 #kf = KFold(n_splits=5, shuffle=False, random_state=None)
 skf = StratifiedKFold(n_splits=3, shuffle=False, random_state=None)
 
@@ -42,3 +87,6 @@ print(all_mse)
 final_mse_mean = np.mean(all_mse, axis=0)
 
 print(final_mse_mean)
+
+'''
+
