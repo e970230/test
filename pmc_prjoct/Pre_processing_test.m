9aec1d281a347f354147ab60782cde387ff0f52f
e970230 <148304931+e970230@users.noreply.github.com>
2024-08-20 07:39:22 +0800

    自創新增選擇法

--- pmc_prjoct/Pre_processing_test.m
+++ pmc_prjoct/Pre_processing_test.m
@@ -5,7 +5,7 @@ close all
 %此為113-PMC螺桿系統動態監測分析與狀態估測模組開發的前程式開發檔案
 
 % 指定資料夾路徑
-folderPath = 'D:\老師資料\113-PMC螺桿系統動態監測分析與狀態估測模組開發\matlab_prjoct'; % 替換成你的資料夾路徑
+folderPath = 'C:\Users\admin\Documents\GitHub\test\pmc_prjoct'; % 替換成你的資料夾路徑
 
 % 獲取資料夾中所有的 CSV 檔案
 matFiles = dir(fullfile(folderPath, '*.mat'));
@@ -36,39 +36,20 @@ data_ylabel = {'g' 'g' 'g' '\mu_m' '\mu_m' 'mm' 'V' 'A'};
 
 % hold off
 %------------------------------------處理負訊號嘗試-------------------------
-find_num = 3;
-
-prcoss_data = init_data{find_num,6};
-for i=1:size(prcoss_data,1)
-    if prcoss_data(i)<=6.285
-        prcoss_data(i)=prcoss_data(i-1);
-    end
-end
-figure(1)
-x = 1:size(prcoss_data,1);
-plot(x,prcoss_data)
-title('將負訊號排除掉後訊號')
-xlabel('Time(sec)')
-ylabel('mm')
-
-figure(2)
-x = 1:size(init_data{find_num,6},1);
-plot(x,init_data{find_num,6})
-title('原始訊號')
-xlabel('Time(sec)')
-ylabel('mm')
-%--------------------------------------------------------------------------
-
-
-% find_num = 2;
+% find_num = 3;
 % 
 % prcoss_data = init_data{find_num,6};
-% 
-% 
-% order = 33;      %濾波器的階數，此值須小於移動窗口的長度
-% framelen = 193;  %移動窗口的長度(必須為奇數
-% 
-% sg_prcoss_data = sgolayfilt(prcoss_data,order,framelen);
+% for i=1:size(prcoss_data,1)
+%     if prcoss_data(i)<=6.285
+%         prcoss_data(i)=prcoss_data(i-1);
+%     end
+% end
+% figure(1)
+% x = 1:size(prcoss_data,1);
+% plot(x,prcoss_data)
+% title('將負訊號排除掉後訊號')
+% xlabel('Time(sec)')
+% ylabel('mm')
 % 
 % figure(2)
 % x = 1:size(init_data{find_num,6},1);
@@ -76,7 +57,70 @@ ylabel('mm')
 % title('原始訊號')
 % xlabel('Time(sec)')
 % ylabel('mm')
+%--------------------------------------------------------------------------
 
+
+find_num = 3;
+
+prcoss_data = init_data{find_num,6};
+
+figure(2)
+x = 1:size(init_data{find_num,6},1);
+plot(x,init_data{find_num,6})
+title('原始訊號')
+xlabel('Time(sec)')
+ylabel('mm')
+
+%-----------------------------------自行開發處------------------------------
+
+normalized_prcoss_data = normalize(prcoss_data, 'range');             %將數據正規化到 [0, 1] 範圍內
+postion_prcoss_data = find(normalized_prcoss_data == 1);             %將正規化後數字超過0.95的位置抓出來
+        
+diff_prcoss_data = diff(postion_prcoss_data);           %得出差值
+ans_01 =[1;find(diff_prcoss_data~=1);size(postion_prcoss_data,1)];                     %把前後差值不是1抓出來，想找出前後不連續的位置
+correct_point = find(diff(ans_01)~=1);
+look_for_point = ans_01(correct_point)+1;
+
+
+
+diff_ans_01 = diff(ans_01);
+rp = 0;
+for i=1:size(diff_ans_01,1)-1
+
+    if diff_ans_01(i+rp)==1
+        if diff_ans_01(i+rp+1)==1
+            rp = rp + 1;
+        end
+        answer(i,:) = [ans_01(i+1+rp)+1 ans_01(i+2+rp)];
+        rp = rp + 1;
+        
+        
+    else
+        answer(i,:) = [ans_01(i+rp)+1 ans_01(i+1+rp)];
+    end
+    
+    look = rp+i;
+
+    if look==size(diff_ans_01,1)
+        answer(1,1)=1;
+        break
+    end
+end
+
+
+final_data = postion_prcoss_data(answer)
+
+
+%--------------------------------------------------------------------------
+
+% order = 3;      %濾波器的階數，此值須小於移動窗口的長度
+% framelen = 193;  %移動窗口的長度(必須為奇數
+% 
+% sg_prcoss_data = sgolayfilt(prcoss_data,order,framelen);
+% [output]=diff_signal(prcoss_data,200,0.1);
+% 
+% 
+% 
 % figure(3)
 % x = 1:size(sg_prcoss_data,1);
 % plot(x,sg_prcoss_data)
